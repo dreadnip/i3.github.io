1e9da6365bc2bccb36e38cf7d1d95a092a58567a
Dreadnip <sander.delamarche@outlook.com>
2020-02-20 20:49:01 +0100

    Remove leftover JS

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-/*
- * The Photoswipe gallery for the screenshot page
- */
-function initGallery(screens) {
-    const pswpElement = document.querySelector('.pswp');
-
-    var options = {
-        index: 0 // start at first slide
-    };
-    console.log(screens);
-    // Initializes and opens PhotoSwipe
-    const gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, screens, options);
-    gallery.init();
-}
 
 /*
  * The "show more" toggle for the downloads table
@@ -40,14 +26,6 @@ function initDownloads(oldVersions) {
  * Our init function
  */
 (function () {
-
-    // If screens are present, init the gallery
-    const screens = document.querySelectorAll('.screen');
-    screens.forEach((image) => {
-        image.addEventListener('click', () => {
-            initGallery(screens);
-        });
-    });
 
     // If the tbody with old versions is in the DOM, init the JS toggle
     const oldVersions = document.querySelector('.old');
