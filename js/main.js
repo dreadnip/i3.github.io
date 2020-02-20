318c4ffdc89d4913231227c3eb4d6ad4f13099a6
Sander De la Marche <sander@sumocoders.be>
2020-02-20 18:40:11 +0100

    WIP: start working on Photoswipe transition

--- js/main.js
+++ js/main.js
@@ -1,27 +1,61 @@
 // vim:ts=4:sw=4:expandtab
 // © 2011 Michael Stapelberg
 
-(function () {
-    const oldVersions = document.querySelector('.old');
+/*
+ * The Photoswipe gallery for the screenshot page
+ */
+function initGallery(screens) {
+    const pswpElement = document.querySelector('.pswp');
+
+    var options = {
+        index: 0 // start at first slide
+    };
+    console.log(screens);
+    // Initializes and opens PhotoSwipe
+    const gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, screens, options);
+    gallery.init();
+}
+
+/*
+ * The "show more" toggle for the downloads table
+ */
+function initDownloads(oldVersions) {
     const oldTrigger = document.querySelector('#showOldDownloads');
     const oldAmount = document.querySelector('#numberOfOldDownloads');
 
-    // If the old versions tbody is found
-    if (oldVersions) {
+    // Display the number of old versions
+    oldAmount.innerHTML = oldVersions.children.length;
 
-        // Display the number of old versions
-        oldAmount.innerHTML = oldVersions.children.length;
+    oldTrigger.addEventListener('click', (event) => {
 
-        oldTrigger.addEventListener('click', (event) => {
+        // Stop the link from redirecting
+        event.preventDefault();
 
-            // Stop the link from redirecting
-            event.preventDefault();
+        // Show the second tbody, the one with the old versions
+        oldVersions.style.display = 'table-row-group';
 
-            // Show the second tbody, the one with the old versions
-            oldVersions.style.display = 'table-row-group';
+        // Hide the expand link
+        oldTrigger.style.display = 'none';
+    });
+}
 
-            // Hide the expand link
-            oldTrigger.style.display = 'none';
+/*
+ * Our init function
+ */
+(function () {
+
+    // If screens are present, init the gallery
+    const screens = document.querySelectorAll('.screen');
+    screens.forEach((image) => {
+        image.addEventListener('click', () => {
+            initGallery(screens);
         });
+    });
+
+    // If the tbody with old versions is in the DOM, init the JS toggle
+    const oldVersions = document.querySelector('.old');
+    if (oldVersions) {
+        initDownloads(oldVersions)
     }
+
 })();
